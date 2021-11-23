f1869ba4a26121f44599f0d84475257ad5feb7c5
Julia Williams <91701494+JuliaWilliams196@users.noreply.github.com>
2021-11-22 19:16:51 -0500

    Add files via upload
    initial commit- added files

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React, { useState } from "react";
+import "./index.css";
+import Flower from "./sunflower.js";
+import ColorPalette from "./ColorPalette.js";
+
+const Colors = () => {
+  const [fillColors, setFillColors] = useState(Array(22).fill('white'))
+  const [currentColor, setCurrentColor] = useState('plum')
+
+  const onFillColor = (i) => {
+    let newFillColors = fillColors.slice(0)
+    newFillColors[i] = currentColor
+    setFillColors(newFillColors)
+  }
+  return (
+    <div className="App">
+      <div className="flower">
+        <Flower fillColors={fillColors} onFill={onFillColor} />
+      </div>
+      <ColorPalette currentColor={currentColor} changeColor={setCurrentColor} />
+    </div>
+  )
+}
+export default Colors
+
